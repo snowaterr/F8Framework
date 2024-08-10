39cfe244c6b1500466f8823ffca584959cf307cb
ZIM <zlmrwp@gmail.com>
2024-08-10 12:46:47 +0800

    将logviewereditor脚本移动到Editor文件夹下

--- Editor/ZIM/LogViewerEditor.cs
+++ Editor/ZIM/LogViewerEditor.cs
@@ -1,5 +1,5 @@
-﻿using log4net.Util;
-using UnityEditor;
+﻿using UnityEditor;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
 namespace F8Framework.Core.Editor
@@ -8,12 +8,12 @@ namespace F8Framework.Core.Editor
     {
         private LogViewerEditor() { }
 
-        [MenuItem("开发工具/添加LogViewer至当前场景", false, 103)]
+        [MenuItem("开发工具/创建LogViewer", false, 103)]
         public static void InstantiatePrefab()
         {
             // 获取当前脚本文件的路径
             string scriptPath = AssetDatabase.GetAssetPath(MonoScript.FromScriptableObject(ScriptableObject.CreateInstance<LogViewerEditor>()));
-            var prefabPath = scriptPath.Replace("/LogViewerEditor.cs", "/LogViewer.prefab");
+            var prefabPath = scriptPath.Replace("Editor/ZIM/LogViewerEditor.cs", "Runtime/Log/Extensions/Prefabs/LogViewer.prefab");
             // 加载预制件
             GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(prefabPath);
 
@@ -21,10 +21,11 @@ namespace F8Framework.Core.Editor
             if (prefab != null)
             {
                 // 实例化预制件
-                var instance = GameObject.Instantiate(prefab);
+                var instance = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+                //var instance = GameObject.Instantiate(prefab);
+                instance.transform.SetAsLastSibling();
                 instance.transform.GetChild(0).gameObject.SetActive(false);
-                instance.name = "LogViewer--F8Framework";
-                LogF8.Log("成功添加LogViewer");
+                LogF8.LogUtil("成功添加 LogViewer 到当前场景");
             }
             else
             {
