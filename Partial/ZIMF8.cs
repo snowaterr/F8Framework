6d024f8ae280f1845dc1706d9b4bb9fdc8f54b94
ZIM <zlmrwp@gmail.com>
2024-09-02 08:45:39 +0800

    for test

---
+++ Partial/ZIMF8.cs
@@ -0,0 +1,11 @@
+﻿using F8Framework.Core;
+using F8Framework.Core.ZIM;
+using F8Framework.F8ExcelDataClass;
+
+namespace F8Framework.Launcher
+{
+    public static partial class ZIMF8
+    {
+        public static int ID = 5;
+    }
+}
